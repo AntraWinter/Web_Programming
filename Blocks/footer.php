14ce39acebc204c00fa6f33d4077c659c4443410
Anastasiya Belousova <104447776+AntraWinter@users.noreply.github.com>
2025-02-18 13:56:08 +0300

    Add files via upload

---
+++ Blocks/footer.php
@@ -0,0 +1,66 @@
+<div class="footer-container">
+    <div class="footer-section">
+        <p class="zagolovki-footer">покупателям:</pp>
+        <ul>
+            <li><a href="gift-certificate.php" class="nav-links">сертификаты и купоны</a></li>
+            <li><a href="https://befree.ru/mobile-apps" class="nav-links">мобильные приложения</a></li>
+            <li><a href="Delivery.php" class="nav-links">доставка</a></li>
+            <li><a href="Payment.php" class="nav-links">оплата</a></li>
+            <li><a href="https://befree.ru/refund" class="nav-links">возврат и обмен</a></li>
+            <li><a href="https://befree.ru/faq" class="nav-links">служба поддержки</a></li>
+            <li><a href="Magazines.php" class="nav-links">магазины</a></li>
+        </ul>
+    </div>
+    <div class="footer-section">
+        <p class="zagolovki-footer">компания:</p>
+        <ul>
+            <li><a href="Onas.php" class="nav-links">о компании</a></li>
+            <li><a href="https://befree.ru/about-the-brand" class="nav-links">о бренде</a></li>
+            <li><a href="https://befree.ru/job" class="nav-links">работа у нас</a></li>
+            <li><a href="https://befree.ru/franchise" class="nav-links">франшиза</a></li>
+            <li><a href="https://befree.ru/wholesale" class="nav-links">опт</a></li>
+            <li><a href="Contacts.php" class="nav-links">контакты</a></li>
+            <li><a href="https://advcake.ru/lp/befree/" class="nav-links">партнерская программа</a></li>
+        </ul>
+    </div>
+    <div class="footer-section">
+        <p class="zagolovki-footer">наше приложение:</p>
+        <div class="app-store">
+            <a href="https://apps.apple.com/ru/app/befree/id1040073468" target="_blank">
+                <img src="Images/DownloadontheAppStore.jpg" alt="App Store">
+            </a>
+        </div>
+        <div class="google-play">
+            <a href="https://play.google.com/store/apps/details?id=com.ddgcorp.befree" target="_blank">
+                <img src="Images/GetitonGooglePlay.jpg" alt="Google Play">
+            </a>
+        </div>
+    </div>
+    <div class="footer-section">
+        <p class="zagolovki-footer">мы в сети:</p>
+        <div class="social-media">
+            <a href="https://t.me/befree_community" target="_blank">
+                <img src="Images/tg.jpg" alt="Telegram">
+            </a>
+            <a href="https://vk.com/befree_fashion" target="_blank">
+                <img src="Images/vk.jpg" alt="Vk">
+            </a>
+            <a href="https://www.tiktok.com/@befree.fashion" target="_blank">
+                <img src="Images/tiktok.jpg" alt="TikTok">
+            </a>
+            <a href="https://www.youtube.com/user/befreenewsite" target="_blank">
+                <img src="Images/yt.jpg" alt="YouTube">
+            </a>
+        </div>
+    </div>
+</div>
+<div class="footer-info">
+    <div class="footer-prava">
+        <a style="color: #a1aaaa;">&copy; 2024 Все права защищены</a><br>
+    </div>
+    <div class="footer-links">
+        <a href="/terms-of-service.php" class="nav-links">условия использования</a>
+        <a href="/user-agreement.php" class="nav-links">пользовательское соглашение</a>
+        <a href="/Privacy-Policy.php" class="nav-links">политика конфиденциальности</a>
+    </div>
+</div>
